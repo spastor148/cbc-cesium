72eb708058bbc5f81f783cdfc312fdb1eb8895bc
chenbincai@four.four-faith.com <982810642@qq.com>
2026-01-06 19:22:33 +0800

    备份

--- public/FFCesiumExample/AdvancedExamples/addType/showMapByAreaLngLat.vue
+++ public/FFCesiumExample/AdvancedExamples/addType/showMapByAreaLngLat.vue
@@ -8,33 +8,10 @@ import { ref, onMounted, nextTick } from "vue";
 import FFCesium from "FFCesium";
 let ffCesium = null;
 onMounted(() => {
-  let viewerOption = {
-    animation: false, //是否创建动画小器件，左下角仪表
-    baseLayerPicker: false, //是否显示图层选择器
-    fullscreenButton: false, //是否显示全屏按钮
-    geocoder: false, //是否显示geocoder小器件，右上角查询按钮
-    homeButton: false, //是否显示Home按钮
-    infoBox: false, //是否显示信息框
-    sceneModePicker: false, //是否显示3D/2D选择器
-    scene3DOnly: false, //如果设置为true，则所有几何图形以3D模式绘制以节约GPU资源
-    selectionIndicator: false, //是否显示选取指示器组件
-    timeline: false, //是否显示时间轴
-    navigationHelpButton: false, //是否显示右上角的帮助按钮
-    shadows: true, //是否显示背影
-    shouldAnimate: true,
-    baseLayer: false,
-    // 透明必须设置true
-    orderIndependentTranslucency: true,
-    contextOptions: {
-      webgl: {
-        alpha: true,
-      },
-    },
-  };
-  ffCesium = new FFCesium("cesiumContainer", viewerOption);
-  ffCesium.addTdtImgLayer();
+  ffCesium = new FFCesium("cesiumContainer");
+  console.log("ffCesium", ffCesium);
   //定位
-  ffCesium.setView({
+  ffCesium.mapActionClass.setView({
     lng: 118.08164379,
     lat: 37.1808099,
     height: 85000,
